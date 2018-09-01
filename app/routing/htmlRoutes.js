466a6a888c8d4aab50f6f9a612a3367fc11d8851
Spencer Vigil <spencervigil27@gmail.com>
2018-08-31 23:49:05 -0500

    added html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var router = new express.Router()
+
+router.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+});
+
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+});
+
+module.exports = router
--- server.js
+++ server.js
@@ -12,14 +12,20 @@ var app = express();
 
 var port = process.env.port || 3000;
 
+// set up body-parser
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static("app"));
 
+// direct to the correct API
+
 app.use("/friends", api);
 
 app.use("/", router);
+
+// Confirm app is listeing on the port
 
 app.listen(port, function () {
     console.log("App listening on port " + port);
